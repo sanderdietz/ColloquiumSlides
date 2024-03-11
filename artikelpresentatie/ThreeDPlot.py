6b3fa36a47ce3b0e11301d1d93001fbb73fbd9d9
sanderdietz <sanderdietz1@gmail.com>
2024-03-11 21:15:34 +0100

    animatie

--- artikelpresentatie/ThreeDPlot.py
+++ artikelpresentatie/ThreeDPlot.py
@@ -8,14 +8,24 @@ class Slide1(ThreeDScene):
             return np.array([0, 0, 0])
         return np.array([u,
                          v,
-                         W * (v * v) / ((u + v) * (u + v)) + w * (u * u) / ((u + v) * (u + v))])
+                         w * u * (1 - v) + W * (1 - u) * v])
 
     def construct(self):
         axes = ThreeDAxes(x_range=[0, 1], y_range=[0, 1], z_range=[0, 1])
         surface = Surface(
             lambda u, v: axes.c2p(*self.function(u, v)),
             u_range=[0, 1],
-            v_range=[0, 1]
+            v_range=[0, 1],
+            resolution=10
         )
-        self.set_camera_orientation(zoom=.5)
-        self.add(axes, surface)
+        labels = axes.get_axis_labels(
+            Text("x"), Text("y"), Text("z")
+        )
+        self.add(axes, surface, labels)
+        # THETA 0 .25 * PI
+        self.set_camera_orientation(phi=.25 * PI, theta=.25 * PI, zoom=.5)
+        self.move_camera(phi=.5 * PI, theta=.25 * PI, zoom=.5)
+        self.move_camera(phi=.5 * PI, theta=-.25 * PI, zoom=.5)
+        # self.wait(.5)
+        # self.begin_ambient_camera_rotation(rate=0.25)
+        # self.wait(10)
