a92d9b2ffab0e9a949418b7e92e1d033ecaa3dc4
sanderdietz <s.dietz@student.tudelft.nl>
2024-03-15 17:28:42 +0100

    refactor

--- artikelpresentatie/TwoDPlot.py
+++ artikelpresentatie/TwoDPlot.py
@@ -1,3 +1,5 @@
+# manim -qh TwoDPlot.py Image01 Image02 Image03 Image04 Image05 Image06 Image07
+
 from manim import *
 
 config.frame_height = 8
@@ -7,14 +9,13 @@ config.frame_size = (1000, 1000)
 PRIMARY = "#00a6d6"
 SECONDARY = "#e03c31"
 
-class Plot(Scene):
+class Base2D(Scene):
     def function(self, x, y):
-        w = 2 / 3
         W = 1
+        w = 2 / 3
         return x * (1 - y) * w + (1 - x) * y * W
 
-    def SetupAxes(self):
-        self.camera.background_color = WHITE
+    def setup_axes(self):
         axes = Axes(
             x_range=[0, 1, .2],
             y_range=[0, 1, .2],
@@ -27,57 +28,50 @@ class Plot(Scene):
         axes.color = BLACK
         labels = axes.get_axis_labels(MathTex(r"y", color=BLACK), MathTex(r"E", color=BLACK))
         return axes, labels
-
-class Image01(Plot):
-    def construct(self):
-        axes, labels = self.SetupAxes()
+    
+    def setup(self):
+        self.camera.background_color = WHITE
+        axes, labels = self.setup_axes()
         self.add(axes, labels)
-        graph = axes.plot(lambda y: self.function(.1, y), color=PRIMARY, stroke_width=8)
-        self.add(graph)
+        return axes
 
-class Image02(Plot):
-    def construct(self):
-        axes, labels = self.SetupAxes()
-        self.add(axes, labels)
-        self.add(axes.plot(lambda y: self.function(.1, y), color=GRAY, stroke_width=2))
-        self.add(axes.plot(lambda y: self.function(.2, y), color=PRIMARY, stroke_width=8))
+    def plot_n_lines(self, axes, n):
+        for i in range(1, n):
+            self.add(axes.plot(lambda y: self.function(.1 * i, y), color=GRAY, stroke_width=2))
+        self.add(axes.plot(lambda y: self.function(.1 * n, y), color=PRIMARY, stroke_width=8))
 
-class Image03(Plot):
-    def construct(self):
-        axes, labels = self.SetupAxes()
-        self.add(axes, labels)
-        for i in [.1, .2]:
-            self.add(axes.plot(lambda y: self.function(i, y), color=GRAY, stroke_width=2))
-        self.add(axes.plot(lambda y: self.function(.3, y), color=PRIMARY, stroke_width=8))
 
-class Image04(Plot):
+class Image01(Base2D):
     def construct(self):
-        axes, labels = self.SetupAxes()
-        self.add(axes, labels)
-        for i in [.1, .2, .3]:
-            self.add(axes.plot(lambda y: self.function(i, y), color=GRAY, stroke_width=2))
-        self.add(axes.plot(lambda y: self.function(.4, y), color=PRIMARY, stroke_width=8))
+        axes = self.setup()
+        self.plot_n_lines(axes, 1)
 
-class Image05(Plot):
+class Image02(Base2D):
     def construct(self):
-        axes, labels = self.SetupAxes()
-        self.add(axes, labels)
-        for i in [.1, .2, .3, .4]:
-            self.add(axes.plot(lambda y: self.function(i, y), color=GRAY, stroke_width=2))
-        self.add(axes.plot(lambda y: self.function(.5, y), color=PRIMARY, stroke_width=8))
+        axes = self.setup()
+        self.plot_n_lines(axes, 2)
 
-class Image06(Plot):
+class Image03(Base2D):
     def construct(self):
-        axes, labels = self.SetupAxes()
-        self.add(axes, labels)
-        for i in [.1, .2, .3, .4, .5]:
-            self.add(axes.plot(lambda y: self.function(i, y), color=GRAY, stroke_width=2))
-        self.add(axes.plot(lambda y: self.function(.6, y), color=PRIMARY, stroke_width=8))
+        axes = self.setup()
+        self.plot_n_lines(axes, 3)
 
-class Image07(Plot):
+class Image04(Base2D):
     def construct(self):
-        axes, labels = self.SetupAxes()
-        self.add(axes, labels)
-        for i in [.1, .2, .3, .4, .5, .6]:
-            self.add(axes.plot(lambda y: self.function(i, y), color=GRAY, stroke_width=2))
-        self.add(axes.plot(lambda y: self.function(.7, y), color=PRIMARY, stroke_width=8))
+        axes = self.setup()
+        self.plot_n_lines(axes, 4)
+
+class Image05(Base2D):
+    def construct(self):
+        axes = self.setup()
+        self.plot_n_lines(axes, 5)
+
+class Image06(Base2D):
+    def construct(self):
+        axes = self.setup()
+        self.plot_n_lines(axes, 6)
+
+class Image07(Base2D):
+    def construct(self):
+        axes = self.setup()
+        self.plot_n_lines(axes, 7)
