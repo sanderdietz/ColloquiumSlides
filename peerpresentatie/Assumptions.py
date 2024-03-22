00b352473cce29bb338419ce3b529798458231bb
sanderdietz <s.dietz@student.tudelft.nl>
2024-03-22 13:31:11 +0100

    dingen

--- peerpresentatie/Assumptions.py
+++ peerpresentatie/Assumptions.py
@@ -1,69 +1,44 @@
-# manim -qh Assumptions.py Image01 Image02 Image03 Image04 Image05 Image06
+# manim -qh Assumptions.py Image01 Image02
 
 from manim import *
-import helper
+from MoserCircle import *
 
-config.frame_height = 8
-config.frame_width = 8
-config.frame_size = (1000, 1000)
+set_frame_size()
 
-BACKGROUND = "#ffffff"
-PRIMARY = "#000000"
-SECONDARY = "#00a6d6"
-TERTIARY = "#e03c31"
-
-R = 3.5
-ANGLE1 = .5 * PI
-ANGLE2 = 1.1 * PI
-ANGLE3 = -.2 * PI
-ANGLE4 = -.6 * PI
-ANGLE5 = .3 * PI
-ANGLE6 = .8 * PI
-
-class Image01(Scene):
+class Image01(MoserCircle):
     def construct(self):
-        self.camera.background_color = BACKGROUND
-        circle = Circle(radius=R, color=PRIMARY)
-        self.add(circle)
-        points = [
-            Dot(circle.point_at_angle(ANGLE1), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE2), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE3), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE4), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE5), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE6), color=PRIMARY)
-        ]
-        for i in range(len(points)):
-            for j in range(i, len(points)):
-                line = Line(points[i], points[j], color=PRIMARY)
-                self.add(line)
-            self.add(points[i])
+        self.circle.color = BLACK
+        self.circle.stroke_width = 4
+        self.add(self.circle)
 
-class Image02(Scene):
-    def construct(self):
-        a1, b1 = helper.linear_angles(R, ANGLE1, ANGLE4)
-        a2, b2 = helper.linear_angles(R, ANGLE3, ANGLE6)
-        intersection_point = helper.intersection(a1, b1, a2, b2)
-        a = (intersection_point[1] - R * np.sin(ANGLE2)) / (intersection_point[0] - R * np.cos(ANGLE2))
-        b = intersection_point[1] - a * intersection_point[0]
-        x = (-a * b + np.sqrt(a * a * b * b - (a * a + 1) * (b * b - R * R))) / (a * a + 1)
-        y = a * x + b
-
-        self.camera.background_color = BACKGROUND
-        circle = Circle(radius=R, color=PRIMARY)
-        self.add(circle)
-        points = [
-            Dot(circle.point_at_angle(ANGLE1), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE2), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE3), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE4), color=PRIMARY),
-            Dot((x, y, 0), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE6), color=PRIMARY)
-        ]
-        for i in range(len(points)):
-            for j in range(i, len(points)):
-                line = Line(points[i], points[j], color=PRIMARY)
-                self.add(line)
-            self.add(points[i])
+        for chord in self.chords:
+            chord.color = BLACK
+            chord.stroke_width = 4
+            self.add(chord)
         
-        self.add(Circle(.25, color=TERTIARY, stroke_width=6).shift(intersection_point))
+        for i, dot in enumerate(self.dots):
+            dot._original__init__(self.points[i], color=BLACK)
+            self.add(dot)
+
+class Image02(MoserCircle):
+    def construct(self):
+        self.circle.color = BLACK
+        self.circle.stroke_width = 4
+        self.add(self.circle)
+
+        intersection_point = self.intersection(self.points[1], self.points[4], self.points[2], self.points[5])
+        alternate_point = self.get_alternate_point(intersection_point)
+        self.points[0] = alternate_point
+        self.dots[0] = Dot(alternate_point, color=GRAY, radius=.05)
+        self.chords = self.calculate_chords()
+
+        for chord in self.chords:
+            chord.color = BLACK
+            chord.stroke_width = 4
+            self.add(chord)
+        
+        for i, dot in enumerate(self.dots):
+            dot._original__init__(self.points[i], color=BLACK)
+            self.add(dot)
+        
+        self.add(Circle(.25, color=SECONDARY, stroke_width=6).shift(intersection_point))
--- peerpresentatie/Intro.py
+++ peerpresentatie/Intro.py
@@ -1,130 +1,63 @@
 # manim -qh Intro.py Image01 Image02 Image03 Image04 Image05 Image06
 
 from manim import *
+from MoserCircle import *
 
-config.frame_height = 8
-config.frame_width = 8
-config.frame_size = (1000, 1000)
+set_frame_size()
 
-BACKGROUND = "#ffffff"
-PRIMARY = "#000000"
-SECONDARY = "#00a6d6"
-TERTIARY = "#e03c31"
+class Intro(MoserCircle):
+    def image(self, chord_indices, dot_indices):
+        self.circle.color = BLACK
+        self.circle.stroke_width = 4
+        self.add(self.circle)
 
-R = 3.5
-ANGLE1 = .5 * PI
-ANGLE2 = 1.1 * PI
-ANGLE3 = -.2 * PI
-ANGLE4 = -.6 * PI
-ANGLE5 = .3 * PI
-ANGLE6 = .8 * PI
-
-class Image01(Scene):
-    def construct(self):
-        self.camera.background_color = BACKGROUND
-        circle = Circle(radius=R, color=PRIMARY)
-        self.add(circle)
-
-class Image02(Scene):
-    def construct(self):
-        self.camera.background_color = BACKGROUND
-        circle = Circle(radius=R, color=PRIMARY)
-        self.add(circle)
-        points = [
-            Dot(circle.point_at_angle(ANGLE1), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE2), color=PRIMARY)
-        ]
-        for i in range(len(points)):
-            for j in range(i, len(points)):
-                line = Line(points[i], points[j], color=PRIMARY)
-                self.add(line)
-            self.add(points[i])
-
-class Image03(Scene):
-    def construct(self):
-        self.camera.background_color = BACKGROUND
-        circle = Circle(radius=R, color=PRIMARY)
-        self.add(circle)
-        points = [
-            Dot(circle.point_at_angle(ANGLE1), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE2), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE3), color=PRIMARY)
-        ]
-        for i in range(len(points)):
-            for j in range(i, len(points)):
-                line = Line(points[i], points[j], color=PRIMARY)
-                self.add(line)
-            self.add(points[i])
-
-class Image04(Scene):
-    def construct(self):
-        self.camera.background_color = BACKGROUND
-        circle = Circle(radius=R, color=PRIMARY)
-        self.add(circle)
-        points = [
-            Dot(circle.point_at_angle(ANGLE1), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE2), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE3), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE4), color=PRIMARY)
-        ]
-        for i in range(len(points)):
-            for j in range(i, len(points)):
-                line = Line(points[i], points[j], color=PRIMARY)
-                self.add(line)
-            self.add(points[i])
-
-class Image05(Scene):
-    def construct(self):
-        self.camera.background_color = BACKGROUND
-        circle = Circle(radius=R, color=PRIMARY)
-        self.add(circle)
-        points = [
-            Dot(circle.point_at_angle(ANGLE1), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE2), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE3), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE4), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE5), color=PRIMARY)
-        ]
-        for i in range(len(points)):
-            for j in range(i, len(points)):
-                line = Line(points[i], points[j], color=PRIMARY)
-                self.add(line)
-            self.add(points[i])
+        for i, chord in enumerate(self.chords):
+            if i in chord_indices:
+                chord.color = BLACK
+                chord.stroke_width = 4
+                self.add(chord)
         
-class Image06(Scene):
-    def construct(self):
-        self.camera.background_color = BACKGROUND
-        circle = Circle(radius=R, color=PRIMARY)
-        self.add(circle)
-        points = [
-            Dot(circle.point_at_angle(ANGLE1), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE2), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE3), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE4), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE5), color=PRIMARY),
-            Dot(circle.point_at_angle(ANGLE6), color=PRIMARY)
-        ]
-        for i in range(len(points)):
-            for j in range(i, len(points)):
-                line = Line(points[i], points[j], color=PRIMARY)
-                self.add(line)
-            self.add(points[i])
+        for i, dot in enumerate(self.dots):
+            if i in dot_indices:
+                dot._original__init__(self.points[i], color=BLACK)
+                self.add(dot)
 
-class Image07(Scene):
+class Image01(Intro):
     def construct(self):
-        self.camera.background_color = SECONDARY
-        circle = Circle(radius=R, color=WHITE)
-        self.add(circle)
-        points = [
-            Dot(circle.point_at_angle(ANGLE1), color=WHITE),
-            Dot(circle.point_at_angle(ANGLE2), color=WHITE),
-            Dot(circle.point_at_angle(ANGLE3), color=WHITE),
-            Dot(circle.point_at_angle(ANGLE4), color=WHITE),
-            Dot(circle.point_at_angle(ANGLE5), color=WHITE),
-            Dot(circle.point_at_angle(ANGLE6), color=WHITE)
-        ]
-        for i in range(len(points)):
-            for j in range(i, len(points)):
-                line = Line(points[i], points[j], color=WHITE)
-                self.add(line)
-            self.add(points[i])
+        self.image({}, {})
+
+class Image02(Intro):
+    def construct(self):
+        self.image({6}, {1, 3})
+
+class Image03(Intro):
+    def construct(self):
+        self.image({6, 8, 13}, {1, 3, 5})
+
+class Image04(Intro):
+    def construct(self):
+        self.image({6, 7, 8, 12, 13, 14}, {1, 3, 4, 5})
+
+class Image05(Intro):
+    def construct(self):
+        self.image({0, 2, 3, 4, 6, 7, 8, 12, 13, 14}, {0, 1, 3, 4, 5})
+
+class Image06(Intro):
+    def construct(self):
+        self.image(range(15), range(6))
+
+class Image07(MoserCircle):
+    def construct(self):
+        self.camera.background_color = PRIMARY
+        self.circle.color = WHITE
+        self.circle.stroke_width = 4
+        self.add(self.circle)
+
+        for i, chord in enumerate(self.chords):
+            chord.color = WHITE
+            chord.stroke_width = 4
+            self.add(chord)
+        
+        for i, dot in enumerate(self.dots):
+            dot._original__init__(self.points[i], color=WHITE)
+            self.add(dot)
